09bb4ae4364f1b66a90a267d228e6985e732300f
ChakiTech <ckinyua475@gmail.com>
2025-03-26 15:36:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// Scroll reveal animation
-const reveals = document.querySelectorAll(".reveal");
 
-function revealOnScroll() {
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const revealTop = reveals[i].getBoundingClientRect().top;
-    const revealPoint = 150;
-
-    if (revealTop < windowHeight - revealPoint) {
-      reveals[i].classList.add("visible");
-    }
-  }
-}
-
-window.addEventListener("scroll", revealOnScroll);
-
-// Initial check in case already in view
-revealOnScroll();
